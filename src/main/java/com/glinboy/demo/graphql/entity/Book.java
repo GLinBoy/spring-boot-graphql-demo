65e3652544d89114252a6298c0bfcfd58dabb43a
GLinBoy <glinboy@live.com>
2024-06-18 20:45:36 +0200

    👌 IMPROVE: Added book entity

---
+++ src/main/java/com/glinboy/demo/graphql/entity/Book.java
@@ -0,0 +1,23 @@
+package com.glinboy.demo.graphql.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public 
+class Book {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "ID", updatable = false, nullable = false)
+    private Long id;
+
+    @Column(length = 256, nullable = false)
+    private String title;
+    
+}
