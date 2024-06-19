846fdaf725d905e9a071f3d7a818404059d357d0
GLinBoy <glinboy@live.com>
2024-06-19 19:21:07 +0200

    📦 NEW: Added Book service impl.

---
+++ src/main/java/com/glinboy/demo/graphql/service/impl/BookServiceImpl.java
@@ -0,0 +1,10 @@
+package com.glinboy.demo.graphql.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.glinboy.demo.graphql.service.BookService;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+}
