0deb0751dd414fefd287567cf0272dfbddf51faf
GLinBoy <glinboy@live.com>
2024-06-19 19:20:55 +0200

    📦 NEW: Added Author service impl.

---
+++ src/main/java/com/glinboy/demo/graphql/service/impl/AuthorServiceImpl.java
@@ -0,0 +1,10 @@
+package com.glinboy.demo.graphql.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.glinboy.demo.graphql.service.AuthorService;
+
+@Service
+public class AuthorServiceImpl implements AuthorService {
+
+}
