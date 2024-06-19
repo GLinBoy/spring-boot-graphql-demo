ab2ba7a2f1f0b817a7d65dd5780eb904a9cc6a86
GLinBoy <glinboy@live.com>
2024-06-19 19:21:22 +0200

    📦 NEW: Added Comment service impl.

---
+++ src/main/java/com/glinboy/demo/graphql/service/impl/CommentServiceImpl.java
@@ -0,0 +1,10 @@
+package com.glinboy.demo.graphql.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.glinboy.demo.graphql.service.CommentService;
+
+@Service
+public class CommentServiceImpl implements CommentService{
+
+}
